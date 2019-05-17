4debe470f0ae1e252ebf17adbfc7380c2f9bb236
ChanakaUOMIT <chanakauomfit@gmail.com>
2019-05-17 14:15:14 +0530

    ADD navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import './App.css';
-import Movies from './components/movies';
+import React from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import "./App.css";
+import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rental from "./components/rental";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import MovieForm from "./components/movieForm";
 
 function App() {
   return (
     <main className="container">
-      <Movies />
-    </main>  
+      <NavBar />
+      <Switch>
+        <Route path="/movies/:id" component={MovieForm} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/customers" component={Customers} />
+        <Route path="/rentals" component={Rental} />
+        <Route path="/not-found" component={NotFound} />
+        <Redirect from="/" exact to="/movies" />
+        <Redirect to="/not-found" />
+      </Switch>
+    </main>
   );
 }
 
 export default App;
- 
